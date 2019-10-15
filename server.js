6adc5699a99aae7f0bc967083817fdc4031f8101
PriteshFullStackCoder <paul.pritesh@gmail.com>
2019-10-16 00:13:17 +0530

    cht

--- server.js
+++ server.js
@@ -24,15 +24,7 @@ client.connect(err => {
  
  mongoose.Promise = global.Promise;
 
-	// Connecting to the database
-	mongoose.connect(dbConfig.url, {
-		useNewUrlParser: true
-	}).then(() => {
-		console.log("Successfully connected to the database");    
-	}).catch(err => {
-		console.log('Could not connect to the database. Exiting now...', err);
-		process.exit();
-	});
+
 
  app.use(logger('dev'));
  app.get('/', (req,res) => {
